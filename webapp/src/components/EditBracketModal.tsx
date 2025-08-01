4cdfec02dc9687dc310c1d0cc743de9dae392f03
Andre <106368574+seigfrieds@users.noreply.github.com>
2025-07-31 22:45:49 -0500

    work out styling some more
    #29

--- webapp/src/components/EditBracketModal.tsx
+++ webapp/src/components/EditBracketModal.tsx
@@ -12,9 +12,9 @@ const sampleData: Song[] = [
   },
   {
     id: "1",
-    title: "Hunter",
-    mainArtistName: "Bjork",
-    imageUrl: "https://i.scdn.co/image/ab67616d0000b27361340a8868455be0d958fb39",
+    title: "My Collection!",
+    mainArtistName: "Autumn",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b2736f2fbcc6cd529a2017f852b1",
   },
 ];
 
@@ -54,6 +54,13 @@ const matches16 = (
   </>
 );
 
+const matches32 = (
+  <>
+    <>{matches16}</>
+    <>{matches16}</>
+  </>
+);
+
 interface Props {
   isOpen: boolean;
   onClose: () => void;
@@ -74,10 +81,10 @@ function EditBracketModal({ isOpen, onClose }: Props) {
     <dialog ref={modalRef} onClose={onClose}>
       <div id="modal-content">
         <div id="modal-header">
-          <p id="header-text">Edit Bracket</p>
-          <Button onClick={onClose} variant="secondary">
+          <Button onClick={onClose} variant="secondary" size="small">
             X
           </Button>
+          <p id="header-text">Edit Bracket</p>
         </div>
         <div id="modal-body">
           <div id="action-bar">
@@ -88,30 +95,92 @@ function EditBracketModal({ isOpen, onClose }: Props) {
               Shuffle
             </Button>
           </div>
-          <div id="bracket">
-            <div id="bracket-16">
-              <div className="round">{matches16}</div>
-              <div className="round">{matches8}</div>
-              <div className="round">{matches4}</div>
-              <div className="round">{matches2}</div>
-            </div>
-            <div id="bracket-16">
-              <div className="round">{matches2}</div>
-              <div className="round">{matches4}</div>
-              <div className="round">{matches8}</div>
-              <div className="round">{matches16}</div>
-            </div>
-            <div id="bracket-16">
-              <div className="round">{matches16}</div>
-              <div className="round">{matches8}</div>
-              <div className="round">{matches4}</div>
-              <div className="round">{matches2}</div>
-            </div>
-            <div id="bracket-16">
-              <div className="round">{matches2}</div>
-              <div className="round">{matches4}</div>
-              <div className="round">{matches8}</div>
-              <div className="round">{matches16}</div>
+          <div id="bracket-container">
+            <div id="bracket-is-32-or-less">
+              <div id="bracket-16">
+                <div className="round">{matches16}</div>
+                <div className="round">{matches8}</div>
+                <div className="round">{matches4}</div>
+                <div className="round">{matches2}</div>
+              </div>
+              <div id="bracket-16">
+                <div className="round">{matches2}</div>
+                <div className="round">{matches4}</div>
+                <div className="round">{matches8}</div>
+                <div className="round">{matches16}</div>
+              </div>
+              <div id="final-match">
+                <div className="match">
+                  <div className="song-winner" key={sampleData[0].id}>
+                    <img className="song-picture" src={sampleData[0].imageUrl} />
+                    <p className="song-title">{sampleData[0].title}</p>
+                  </div>
+                  <div className="song-loser" key={sampleData[1].id}>
+                    <img className="song-picture" src={sampleData[1].imageUrl} />
+                    <p className="song-title">{sampleData[1].title}</p>
+                  </div>
+                </div>
+              </div>
+              {/* <div id="bracket-32">
+                <div className="round">{matches32}</div>
+                <div className="round">{matches16}</div>
+                <div className="round">{matches8}</div>
+                <div className="round">{matches4}</div>
+                <div className="round">{matches2}</div>
+              </div>
+              <div id="bracket-32">
+                <div className="round">{matches2}</div>
+                <div className="round">{matches4}</div>
+                <div className="round">{matches8}</div>
+                <div className="round">{matches16}</div>
+                <div className="round">{matches32}</div>
+              </div>
+              <div id="bracket-32">
+                <div className="round">{matches32}</div>
+                <div className="round">{matches16}</div>
+                <div className="round">{matches8}</div>
+                <div className="round">{matches4}</div>
+                <div className="round">{matches2}</div>
+              </div>
+              <div id="bracket-32">
+                <div className="round">{matches2}</div>
+                <div className="round">{matches4}</div>
+                <div className="round">{matches8}</div>
+                <div className="round">{matches16}</div>
+                <div className="round">{matches32}</div>
+              </div>
+              <div id="final-four">
+                <div className="match">
+                  <div className="song-winner" key={sampleData[0].id}>
+                    <img className="song-picture" src={sampleData[0].imageUrl} />
+                    <p className="song-title">{sampleData[0].title}</p>
+                  </div>
+                  <div className="song-loser" key={sampleData[1].id}>
+                    <img className="song-picture" src={sampleData[1].imageUrl} />
+                    <p className="song-title">{sampleData[1].title}</p>
+                  </div>
+                </div>
+                <div className="match">
+                  <div className="song-winner" key={sampleData[0].id}>
+                    <img className="song-picture" src={sampleData[0].imageUrl} />
+                    <p className="song-title">{sampleData[0].title}</p>
+                  </div>
+                  <div className="song-loser" key={sampleData[1].id}>
+                    <img className="song-picture" src={sampleData[1].imageUrl} />
+                    <p className="song-title">{sampleData[1].title}</p>
+                  </div>
+                </div>
+                <div className="match">
+                  <div className="song-winner" key={sampleData[0].id}>
+                    <img className="song-picture" src={sampleData[0].imageUrl} />
+                    <p className="song-title">{sampleData[0].title}</p>
+                  </div>
+                  <div className="song-loser" key={sampleData[1].id}>
+                    <img className="song-picture" src={sampleData[1].imageUrl} />
+                    <p className="song-title">{sampleData[1].title}</p>
+                  </div>
+                </div>
+              </div> */}
             </div>
           </div>
         </div>
